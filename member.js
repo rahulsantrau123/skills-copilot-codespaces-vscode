0c40601d98a733a80c52daec650c0f30c8038388
Rahul Santra <99420549+rahulsantrau123@users.noreply.github.com>
2025-04-18 03:54:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+	// Add your implementation here
+    // This function should return an array of skills
+    // that the member has.
+    // For example:
+    // return ['JavaScript', 'Python', 'HTML', 'CSS'];
+    return ['JavaScript', 'Python', 'HTML', 'CSS'];
+}
